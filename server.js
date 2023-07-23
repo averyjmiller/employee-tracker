c1e50f3c26786ea775c84a00d4a1422663ddc65c
Avery-lab <54604339+Avery-lab@users.noreply.github.com>
2023-07-22 21:00:14 -0400

    server file calls init function to start inquirer

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mysql = require('mysql2');
 const init = require('./src/utils/prompt');
 
 const PORT = process.env.PORT || 3001;
@@ -7,16 +6,6 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: 'root',
-    password: 'ci1on3f5O_8Lbrlwrore',
-    database: 'company_db'
-  },
-  console.log(`Connected to the company_db database.`)
-);
 
 init();
 
